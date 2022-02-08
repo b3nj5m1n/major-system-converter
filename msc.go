be0f40fee473ce814af6f42212b2085b4feb7a4c
b3nj4m1n <b3nj4m1n@gmx.net>
2022-02-08 18:08:50 +0100

    CamelCase

--- msc.go
+++ msc.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	height_results = 8
+	heightResults = 8
 )
 
-var style_error = lipgloss.NewStyle().
+var styleError = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color("#FAFAFA")).
 	Background(lipgloss.Color("#d14774"))
@@ -40,7 +40,7 @@ func (i *arrayFlags) Set(value string) error {
 }
 
 var (
-	dataset_files arrayFlags
+	datasetFiles arrayFlags
 )
 
 type Word struct {
@@ -73,15 +73,15 @@ type Result struct {
 }
 
 type Model struct {
-	all_words      AllWords
-	result_words   ResultWords
-	selected_words SelectedWords
-	loading        bool
-	input          textinput.Model
-	spinner        spinner.Model
-	width          int
-	scroll         int
-	result         Result
+	allWords      AllWords
+	resultWords   ResultWords
+	selectedWords SelectedWords
+	loading       bool
+	input         textinput.Model
+	spinner       spinner.Model
+	width         int
+	scroll        int
+	result        Result
 }
 
 func initialModel() Model {
@@ -98,27 +98,27 @@ func initialModel() Model {
 	s.Spinner = spinners[index]
 
 	return Model{
-		all_words:      nil,
-		result_words:   nil,
-		selected_words: nil,
-		loading:        true,
-		input:          ti,
-		spinner:        s,
-		result:         Result{array: nil, lines: 0},
+		allWords:      nil,
+		resultWords:   nil,
+		selectedWords: nil,
+		loading:       true,
+		input:         ti,
+		spinner:       s,
+		result:        Result{array: nil, lines: 0},
 	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return tea.Batch(m.spinner.Tick, load_words)
+	return tea.Batch(m.spinner.Tick, loadWords)
 }
 
-func load_words() tea.Msg {
-	var word_list AllWords
+func loadWords() tea.Msg {
+	var wordList AllWords
 
-	for _, path := range dataset_files {
+	for _, path := range datasetFiles {
 		f, err := os.Open(path)
 		if err != nil {
-			fmt.Println(style_error.Render("Error reading file."))
+			fmt.Println(styleError.Render("Error reading file."))
 			return tea.Quit
 		}
 
@@ -131,11 +131,11 @@ func load_words() tea.Msg {
 				break
 			}
 			if err != nil {
-				fmt.Println(style_error.Render("Error reading file."))
+				fmt.Println(styleError.Render("Error reading file."))
 				return tea.Quit
 			}
-			frequency, conv_err := strconv.Atoi(record[4])
-			if conv_err != nil {
+			frequency, convErr := strconv.Atoi(record[4])
+			if convErr != nil {
 				frequency = 0
 			}
 			word := Word{
@@ -145,16 +145,16 @@ func load_words() tea.Msg {
 				phonemes:  record[3],
 				frequency: frequency,
 			}
-			word_list = append(word_list, word)
+			wordList = append(wordList, word)
 		}
 	}
-	return word_list
+	return wordList
 }
 
-func find_words(number string, search_list WordList) tea.Cmd {
+func findWords(number string, searchList WordList) tea.Cmd {
 	return func() tea.Msg {
 		var result ResultWords
-		for _, word := range search_list {
+		for _, word := range searchList {
 			if word.number == number {
 				result = append(result, word)
 			}
@@ -183,28 +183,28 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "down", "j":
 			m.scroll++
 		case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "backspace":
-			m.input, cmd = m.input.Update(msg)
+			m.input, _ = m.input.Update(msg)
 			if m.input.Value() == "" {
-				m.result_words = nil
+				m.resultWords = nil
 				return m, nil
 			} else {
-				return m, find_words(m.input.Value(), WordList(m.all_words))
+				return m, findWords(m.input.Value(), WordList(m.allWords))
 			}
 		default:
 			return m, nil
 		}
-		return m, format_word_list(m)
+		return m, formatWordList(m)
 	case spinner.TickMsg:
 		m.spinner, cmd = m.spinner.Update(msg)
 		return m, cmd
 	case AllWords:
-		m.all_words = msg
+		m.allWords = msg
 		m.loading = false
 		return m, nil
 	case ResultWords:
-		m.result_words = msg
+		m.resultWords = msg
 		m.scroll = 0
-		return m, format_word_list(m)
+		return m, formatWordList(m)
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		return m, nil
@@ -213,8 +213,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if m.scroll < 0 {
 			m.scroll = 0
 		}
-		if (m.result.lines - (height_results / 2)) < m.scroll {
-			m.scroll = m.result.lines - (height_results / 2)
+		if (m.result.lines - (heightResults / 2)) < m.scroll {
+			m.scroll = m.result.lines - (heightResults / 2)
 		}
 		return m, nil
 	default:
@@ -222,7 +222,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 }
 
-func frequenyc_style(w Word) lipgloss.Style {
+func frequencyStyle(w Word) lipgloss.Style {
 	// 500 most common words
 	if w.frequency >= 470945 {
 		return lipgloss.NewStyle().
@@ -247,8 +247,8 @@ func frequenyc_style(w Word) lipgloss.Style {
 		Faint(true)
 }
 
-func format_word(w Word) string {
-	style := frequenyc_style(w)
+func formatWord(w Word) string {
+	style := frequencyStyle(w)
 	word := w.word
 	switch w.pos {
 	case "JJ":
@@ -305,92 +305,90 @@ func format_word(w Word) string {
 		Render(word)
 }
 
-func format_word_list(m Model) tea.Cmd {
+func formatWordList(m Model) tea.Cmd {
 	return func() tea.Msg {
 		result := ""
-		for _, word := range m.result_words {
-			result += format_word(word) + ", "
+		for _, word := range m.resultWords {
+			result += formatWord(word) + ", "
 		}
 		result = wrap.String(wordwrap.String(result, m.width*2/3), m.width*2/3)
-		result_split := strings.Split(result, "\n")
-		result_len := len(result_split)
-		return Result{array: result_split, lines: result_len}
+		resultSplit := strings.Split(result, "\n")
+		resultLen := len(resultSplit)
+		return Result{array: resultSplit, lines: resultLen}
 	}
 }
 
-func scroll_result(result Result, scroll int) string {
-	result_str := ""
-	for i := scroll; i < scroll+height_results; i++ {
+func scrollResult(result Result, scroll int) string {
+	resultStr := ""
+	for i := scroll; i < scroll+heightResults; i++ {
 		if i >= result.lines {
-			result_str += "\n"
+			resultStr += "\n"
 		} else {
-			result_str += result.array[i] + "\n"
+			resultStr += result.array[i] + "\n"
 		}
 	}
-	return result_str
+	return resultStr
 }
 
 func (m Model) View() string {
 	style := lipgloss.NewStyle().
 		MaxWidth(m.width - (3 * 2))
-	style_container_input := lipgloss.NewStyle().
+	styleContainer := lipgloss.NewStyle().
 		BorderStyle(lipgloss.RoundedBorder()).
 		BorderForeground(lipgloss.Color("#874BFD")).
 		Padding(1, 1).
 		MarginTop(2).
 		Width(m.width / 2)
-	style_container_results := lipgloss.NewStyle().
+	styleContainerResults := lipgloss.NewStyle().
 		Width(m.width * 2 / 3).
 		MaxWidth(m.width * 2 / 3).
-		MaxHeight(height_results)
+		MaxHeight(heightResults)
 
-	s := fmt.Sprintf("")
+	s := ""
 
-	input_str := fmt.Sprintf("")
+	inputStr := ""
 	if m.loading {
-		input_str += m.spinner.View()
-		input_str += " Loading dataset"
+		inputStr += m.spinner.View()
+		inputStr += " Loading dataset"
 	} else {
-		input_str += m.input.View()
+		inputStr += m.input.View()
 	}
 	input := lipgloss.Place(m.width, 0,
 		lipgloss.Center, lipgloss.Center,
-		style_container_input.Render(input_str),
+		styleContainer.Render(inputStr),
 	)
 	s += input
 
 	results := ""
-	if m.result_words != nil {
-		results = style_container_results.Render(scroll_result(m.result, m.scroll))
-	} else if _, err := strconv.Atoi(m.input.Value()); err != nil && !m.loading {
-		// results = style_error.Render("Invalid input.")
+	if m.resultWords != nil {
+		results = styleContainerResults.Render(scrollResult(m.result, m.scroll))
 	}
 	s += lipgloss.PlaceHorizontal(m.width, lipgloss.Center, results)
 	s += "\n"
 
-	var style_help = lipgloss.NewStyle().
+	var styleHelp = lipgloss.NewStyle().
 		Bold(true).
 		Foreground(lipgloss.Color("#708090")).
 		MarginTop(2).
 		UnsetBackground()
-	help := style_help.Render(wordwrap.String("q to quit, j or up arrow to go up, k or down arrow to go down", m.width))
+	help := styleHelp.Render(wordwrap.String("q to quit, j or up arrow to go up, k or down arrow to go down", m.width))
 	s += lipgloss.PlaceHorizontal(m.width, lipgloss.Center, help)
 
 	return style.Render(s)
 }
 
 func main() {
-	flag.Var(&dataset_files, "d", "Dataset file(s) to use.")
+	flag.Var(&datasetFiles, "d", "Dataset file(s) to use.")
 	flag.Parse()
 
-	if len(dataset_files) < 1 {
-		fmt.Println(style_error.Render("Specify at least one dataset."))
+	if len(datasetFiles) < 1 {
+		fmt.Println(styleError.Render("Specify at least one dataset."))
 		os.Exit(1)
 	}
 
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
-		fmt.Println(style_error.Render("Alas, there's been an error."))
+		fmt.Println(styleError.Render("Alas, there's been an error."))
 		os.Exit(1)
 	}
 }
